1da0fb97fa86d33b15ce7946e197721960350d9f
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-02-17 02:34:06 +0000

    formatted run.py

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('The Ironworks')
 
+
 def get_sales_data():
     """ sales data input from user """
     while True:
@@ -20,13 +21,12 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60,70,80,90\n")
         data_str = input("Enter data here:\n")
         sales_data = data_str.split(",")
-    
         if validate_data(sales_data):
             print("Data is Valid!")
             break
-        
     return sales_data
-    
+
+
 def validate_data(values):
     """ create value error if not enough numbers """
     try:
@@ -38,8 +38,8 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, Please try again.\n")
         return False
-    
     return True
+
 
 def update_worksheet(data, worksheet):
     """ function to update the correct worksheet """
@@ -48,43 +48,41 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n") 
            
+           
 def calculate_surplus_data(sales_row):
     """ compare sales and stock to define surplusb """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("Stock").get_all_values()
     stock_row = stock[-1]
-    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-        
     return surplus_data
+
 
 def get_last_5_entries_sales():
     """ merge sales data into stock data function """
     sales = SHEET.worksheet("Sales")
-    
     columns = []
     for ind in range(1, 10):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    
     return columns
     
+
 def calculate_stock_data(data):
     """ calculate stock average, adding 10% """
     print("Calculating stock data...\n")
     new_stock_data = []
-    
     for column in data:
         int_column = [int(num) for num in column]
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-        
     return new_stock_data
     
+
 def main():
     """ program functions """
     data = get_sales_data()
